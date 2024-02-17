dfda4f2d668cd34a1a9c5874309da5fcc7109ec4
Juan <alatorre03@outlook.com>
2024-02-16 20:21:30 -0600

    Initial Commit

---
+++ actividad4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  actividad4
+//
+//  Created by Juan Miguel Flores on 14/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
